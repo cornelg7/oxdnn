ece4ec18cc343339cac4c4bc49edae5732542945
Ivo Maffei <ivomaffei@gmail.com>
2018-04-07 16:04:24 +0200

    played a bit with upload; now need php support

--- WEB/JS/Upload.js
+++ WEB/JS/Upload.js
@@ -2,17 +2,19 @@
 
 //-- HANDLE FILES -----------------------------------------
 
+//handle picture by displaying thumbnail and adding properties
 const handlePicture = function(files) {
 	
 	var previewDiv = document.getElementById("dropZone")
 	
-	//get and display thubnails
+	//get and display thumbnails
 	for (var i =0; i< files.length; i++) {
 		var file = files[i]
 	
 		var image = document.createElement("img")
-		//image.classList.add("obj")
 		image.file = file;
+		image.classList.add("toBeUploaded") //this class will be used to select the file that needs uploading
+		
 		previewDiv.appendChild(image)
 		
 		var reader = new FileReader()
@@ -23,6 +25,60 @@ const handlePicture = function(files) {
 	
 }
 
+
+//function to upload a single file
+function FileUpload(image, file) {
+
+	var xhr = new XMLHttpRequest(); //object to send the data
+	var formData = new FormData();
+	
+	formData.append('picture', file);//add prefix to file
+  /*  I commented out the code to show upload progress; let's first make the upload work 
+  	var self = this;
+  	
+  	//this says that when the upload progress we update the throbber object
+	xhr.upload.addEventListener("progress", function(event) {
+    		if (event.lengthComputable) {
+          		var percentage = Math.round((event.loaded * 100) / event.total);
+          		document.getElementById("progressBar").value=percentage;
+        	}
+    	}, false);
+  	
+  	
+  	//this says that when the upload is complete we set the throbber to 100% and remove it
+	xhr.upload.addEventListener("load", function(e){
+    		self.bar.value=100;
+    		document.getElementById("progressBar").style.display='none'
+    	}, false);
+    */
+    
+    //if something goes wrong
+    xhr.onreadystatechange = function() {
+                if (xhr.readyState == 4 && xhr.status == 200) {
+                    alert(xhr.responseText);
+                }
+    };
+    
+    //here we open the request to upload, the second argument is the .php file to call on the server
+	xhr.open("POST", "file.php", true);
+	xhr.send(formData);
+
+}
+
+
+
+// function which individually sends each picture
+const sendFiles = function () {
+	
+	var images = document.querySelectorAll(".toBeUploaded") //get all pictures
+
+	//upload each picture
+	for (var i =0; i< images.length; i++) {
+		new FileUpload(images[i], images[i].file)
+	
+	}
+
+}
 
 
 //---------------------------------------------------------
@@ -52,15 +108,8 @@ let check = function (files) {
         			
 	}
 	
-	//alert('Uploading done successfully')
 	return true
 };
-
-
-
-
-//now call the check function whenever something is submitted through the form
-document.forms[0].addEventListener('submit', function (event) { if(!check(document.getElementById('fileUploading').files)) {event.preventDefault} else {handlePicture(document.getElementById('fileUploading').files)}}, false );
 
 //--------------------------------------------------------------
 
@@ -110,8 +159,29 @@ dropSection = document.getElementById("dropZone") //retrive the html element
 dropSection.addEventListener("dragenter",dragenter,false)
 dropSection.addEventListener("dragover",dragover,false)
 dropSection.addEventListener("drop",drop,false)
+document.getElementById("RunButton").addEventListener("click", sendFiles, false)
 
 
 //---------------------------------------------------------
 
+
+//-- FORM UPLOAD SECTION -----------------------------------
+
+const formUpload = function (event) {
+
+	var files = document.getElementById('fileUploading').files //get files from form
+	
+	if(!check(files)) { //if the check does not go well
+		event.preventDefault
+	} else {
+		handlePicture(files)
+	}
+
+}
+
+
+//make the element call this function
+document.getElementById('fileUploading').addEventListener("change", formUpload, false)
+
+//----------------------------------------------------------
 

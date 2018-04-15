84af7b84201f1b8ab16556e8aa942468f981ba92
Wojciech Szwarc <wojciech.szwarc@stcatz.ox.ac.uk>
2018-04-15 18:40:47 +0100

    Added server's own security size check and actually reads the uploaded file's data.

--- API/api.js
+++ API/api.js
@@ -1,8 +1,39 @@
-var express = require('express');
-var router = express.Router();
+'use strict';
+const express = require('express');
+const fs = require('fs');
+const busboy = require('connect-busboy');
+const router = express.Router();
 
-router.post('/upload', function(req, res) {    
-    res.send("Success - file uploaded");
+const SIZE_LIMIT = 10 * 1024 * 1024;
+
+router.use(busboy({
+    limits: {
+        fileSize: SIZE_LIMIT,
+        files: 1,
+        fields: 10,
+    },
+}));
+
+function generateNNResponse(picture) {
+    return "Possibly accessible";
+}
+
+router.post('/upload', function(req, res) {
+    req.pipe(req.busboy);
+    req.busboy.on('file', function (fieldname, file, filename) {
+        let fstream = fs.createWriteStream(__dirname + '/temp/' + filename);
+        file.pipe(fstream);
+        fstream.on('close', function () {
+            fs.unlink(fstream.path);    // just a test
+            if (file.truncated) {
+                console.log('Rejected: ' + filename + ' (too large)');
+                return res.status(413).send('The file is too large!');
+            }
+            console.log('Accepted: ' + filename);
+            let response = generateNNResponse("File data");
+            res.send(response);
+        });
+    });
 });
 
 module.exports = router

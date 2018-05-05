5b39a8080794b10ce96540e183ae686e755207f1
lwatt <laurencewatts@hotmail.co.uk>
2018-05-05 20:44:57 +0100

    Reverted to support old keras-retinanet version and fixed ZeroRPC bug

--- ML/app.py
+++ ML/app.py
@@ -2,24 +2,30 @@ import os
 import zerorpc
 import keras
 import logging
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
-from keras_retinanet import models
+from keras_retinanet.models.resnet import custom_objects
 
-from .utils.evaluate import evaluate
+from utils.evaluate import evaluate
 
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+model_path = os.path.join(
+    '.', 'trained_snapshots', 'resnet50_csv_02.h5'
+)
+model = keras.models.load_model(
+    model_path,
+    custom_objects=custom_objects
+)
+
+logger.info('Loaded model successfully')        
 
 class EvaluationRPC(object):
     def __init__(self):
-        model_path = os.path.join(
-            '.', 'trained_snapshots', 'resnet50_csv_02.h5'
-        )
-        self.model = models.load_model(
-            model_path,
-            backbone_name='resnet50'
-        )
-        logger.info('Loaded model successfully')
+        logger.info('Started Python evaluation server')
 
     def evaluate(self, filename):
         image_path = os.path.join(
@@ -29,7 +35,7 @@ class EvaluationRPC(object):
 
         evaluation = evaluate(
             image_path,
-            self.model,
+            model,
             return_image=annotate_image
         )
 
